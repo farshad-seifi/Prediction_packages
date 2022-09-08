88edd8d35167be747e2f8d803d51592226859de6
Farshad Seifi <32601295+farshad-seifi@users.noreply.github.com>
2022-09-08 12:30:04 +0430

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -8,14 +8,14 @@ from darts.utils.timeseries_generation import datetime_attribute_timeseries
 from darts.utils.likelihood_models import GaussianLikelihood
 
 ## Models parameters that give from user
-number_of_step_ahead = 30  # Number of periods that user want to predict
-Monthly_seasonality = True  # If user sets Monthly_seasonality True then model fits Monthly seasonality to data
-Weekly_seasonality = False  # If user sets Weekly_seasonality True then model fits Weekly seasonality to data
-Confidence_limit = True  # Is this parameter be Ture model generates LCL and UCL in addition to prediction
-frequency = "Monthly"  # Data level of prediction -- Allowed values : Monthly & Daily
-RNN_Type = "LSTM"  # If user selects RNN-Based model as a main model, he could select Model type between RNN, LSTM, and GRU
-Direction = "postdict"  # If user selects postdict model removes number of selected step from data and then predict without them.
-# After that user can see the accuarcy of prediction. Another choice for this variable is predict which doesn't have accuracy option.
+number_of_step_ahead = 30       # Number of periods that user want to predict
+Monthly_seasonality = True      # If user sets Monthly_seasonality True then model fits Monthly seasonality to data
+Weekly_seasonality = False      # If user sets Weekly_seasonality True then model fits Weekly seasonality to data
+Confidence_limit = True         # Is this parameter be Ture model generates LCL and UCL in addition to prediction
+frequency = "Monthly"           # Data level of prediction -- Allowed values : Monthly & Daily
+RNN_Type = "LSTM"               # If user selects RNN-Based model as a main model, he could select Model type between RNN, LSTM, and GRU
+Direction = "postdict"          # If user selects postdict model removes number of selected step from data and then predict without them.
+                                # After that user can see the accuarcy of prediction. Another choice for this variable is predict which doesn't have accuracy option.
 
 
 ## Removing last #number_of_step_ahead from data if Direction be postdict
