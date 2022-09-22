ed144e594bc776941f6d979a6b2f7108325dd68d
Farshad Seifi <32601295+farshad-seifi@users.noreply.github.com>
2022-09-22 15:59:14 +0330

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -1,24 +1,22 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
 from darts import TimeSeries
-from darts.dataprocessing.transformers import Scaler, MissingValuesFiller
-from darts.utils.timeseries_generation import datetime_attribute_timeseries
-from darts.utils.likelihood_models import GaussianLikelihood
+import jdatetime
 from darts.models import RandomForest
+from darts.dataprocessing.transformers import Scaler, MissingValuesFiller
+from datetime import datetime, timedelta
 
 
-data = pd.read_excel(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\test_data.xlsx")
+data = pd.read_excel(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\test_data_month.xlsx")
+
+
+data.columns = ["date", "value"]
 
 ## Models parameters that give from user
-number_of_step_ahead = 30       # Number of periods that user want to predict
-Monthly_seasonality = True      # If user sets Monthly_seasonality True then model fits Monthly seasonality to data
-Weekly_seasonality = False      # If user sets Weekly_seasonality True then model fits Weekly seasonality to data
-Confidence_limit = True         # Is this parameter be Ture model generates LCL and UCL in addition to prediction
-frequency = "Monthly"           # Data level of prediction -- Allowed values : Monthly & Daily
-RNN_Type = "LSTM"               # If user selects RNN-Based model as a main model, he could select Model type between RNN, LSTM, and GRU
-Direction = "postdict"          # If user selects postdict model removes number of selected step from data and then predict without them.
-                                # After that user can see the accuarcy of prediction. Another choice for this variable is predict which doesn't have accuracy option.
+number_of_step_ahead = 30   # Number of periods that user want to predict
+Confidence_limit = True     # Is this parameter be Ture model generates LCL and UCL in addition to prediction
+Direction = "postdict"      # If user selects postdict model removes number of selected step from data and then predict without them.
+                            # After that user can see the accuarcy of prediction. Another choice for this variable is predict which doesn't have accuracy option.
 
 
 ## Removing last #number_of_step_ahead from data if Direction be postdict
@@ -28,6 +26,38 @@ if Direction == "postdict":
 else:
     train_data = data
 
+def date_handler(input):
+    date = input
+    try:
+        if len(str(date)) < 8:
+            if "/" in str(date):
+                date = str(date) + "/01"
+            elif "-" in str(date):
+                date = str(date) + "-01"
+            else:
+                date = str(date) + "01"
+        if "/" in str(date) or "-" in str(date):
+            date = str(date).replace("-" , "").replace("/" , "")
+        if int(str(date)[0:4]) < 1500:
+            eng_date = pd.to_datetime(jdatetime.date(year = int(str(date)[0:4]), month = int(str(date)[4:6]) , day = int(str(date)[6:8])).togregorian())
+        if int(str(date)[0:4]) > 1500:
+            eng_date = pd.to_datetime(str(date))
+        return eng_date
+    except:
+        return input
+
+
+def frequency_finder(data):
+    if((data["date"].iloc[0].hour + data["date"].iloc[1].hour) > 0 ):
+        frequency = "Hourly"
+    elif((data["date"].iloc[1]-data["date"].iloc[0]).days == 1):
+        frequency = "Daily"
+    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 27) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 32)):
+        frequency = "Monthly"
+    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 363) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 366)):
+        frequency = "Yearly"
+
+    return frequency
 
 def monthdelta(date, delta):
     # This function calculates the difference between to date based on month
@@ -39,17 +69,20 @@ def monthdelta(date, delta):
 
     return date.replace(day=d, month=m, year=y)
 
-  def RandomForest_Model(data, number_of_step_ahead, frequency):
+
+def RandomForest_Model(data, number_of_step_ahead):
 
     """
-    The main function for RandomForest prediction
+    The main function for NBEATS prediction
     :param data: The main data for prediction
     :param number_of_step_ahead: Number of step ahead for prediction
-    :param frequency: Type of date. Monthly or Daily
     :return: A 4*number_of_step_ahead dimension dataframe
     """
-
+    # Creating the well fitted formated based on the type of frequency
     # Handling dates format of darts
+
+    frequency = frequency_finder(data)
+
     for i in range(0, len(data["date"])):
         if (frequency == "Monthly"):
             year = (monthdelta(datetime.today(), -i)).year
@@ -61,30 +94,16 @@ def monthdelta(date, delta):
             month = (datetime.today() - timedelta(days=i)).month
             day = (datetime.today() - timedelta(days=i)).day
             data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
+        elif (frequency == "Hourly"):
+            year = (datetime.today() - timedelta(hours=i)).year
+            month = (datetime.today() - timedelta(hours=i)).month
+            day = (datetime.today() - timedelta(hours=i)).day
+            hour = (datetime.today() - timedelta(hours=i)).hour
+            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":00:00"
 
     series = TimeSeries.from_dataframe(data, 'date', 'value').astype(np.float32)
     transformer = Scaler()
     series = transformer.fit_transform(series)
-
-    date = pd.DataFrame(np.zeros((len(data["date"]) + number_of_step_ahead, 1)))
-
-    for i in range(0, len(data["date"]) + number_of_step_ahead):
-        if (frequency == "Monthly"):
-            year = (monthdelta(datetime.today(), number_of_step_ahead - i)).year
-            month = (monthdelta(datetime.today(), number_of_step_ahead - i)).month
-            day = (monthdelta(datetime.today(), number_of_step_ahead - i)).day
-            date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month)
-
-        elif (frequency == "Daily"):
-            year = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).year
-            month = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).month
-            day = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).day
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
-            date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
-
-    date.columns = ["date"]
-    date["value"] = 0
-    series_date = TimeSeries.from_dataframe(date, 'date', 'value').astype(np.float32)
 
     #HPO part for reaching better response time
     # parameters = {"lags": [int(np.floor(len(data) / 10)), int(np.floor(len(data) / 5)), int(np.floor(len(data) / 3))]}
@@ -95,14 +114,19 @@ def monthdelta(date, delta):
 
 
     # my_model = RandomForest(lags=best_model[1]["lags"], n_estimators= 30)
+
     my_model = RandomForest(lags= int(np.floor(len(data) / 3)) , n_estimators= 100)
 
-    my_model.fit(series)
-    prediction = my_model.predict(number_of_step_ahead, num_samples = 1)
 
+    my_model.fit(series)
+    prediction = my_model.predict(number_of_step_ahead, num_samples=1)
     forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
     forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
     forecast[1] = pd.DataFrame(transformer.inverse_transform(prediction).values())
     forecast.columns = ["date", "prediction", "LCL", "UCL"]
 
     return forecast
+
+
+train_data['date'] = train_data['date'].apply(date_handler)
+results = RandomForest_Model(train_data, number_of_step_ahead)
