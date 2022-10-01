4eeeb0181ebe49fb4926727734438d6d3f6f56b1
Farshad Seifi <32601295+farshad-seifi@users.noreply.github.com>
2022-10-01 14:12:58 +0330

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -10,6 +10,10 @@ from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from keras.utils.np_utils import to_categorical
+
 
 
 train_data = pd.read_csv(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\kaggle\Titanic\train - Copy.csv")
@@ -40,6 +44,34 @@ def Missing_values_Handler(data, column_name):
         data[column_name].fillna(data[column_name].value_counts().idxmax(), inplace=True)
     else:
         data[column_name].fillna(data[column_name].mean(), inplace=True)
+
+def Outlier_Handler(data, column_name):
+    IQR_outliers = []
+    sorted_data = sorted(data[column_name])
+    q1 = np.percentile(sorted_data, 25)
+    q3 = np.percentile(sorted_data, 75)
+    IQR = q3 - q1
+    lwr_bound = q1 - (1.5 * IQR)
+    upr_bound = q3 + (1.5 * IQR)
+    for i in data[column_name]:
+        if ((i < lwr_bound) or (i > upr_bound)):
+            IQR_outliers.append(i)
+
+    Zscore_outliers = []
+    lcl = data[column_name].mean() - 2.64 * data[column_name].std()
+    ucl = data[column_name].mean() + 2.64 * data[column_name].std()
+
+    for i in data[column_name]:
+        if ((i < lcl) or (i > ucl)):
+            Zscore_outliers.append(i)
+
+    non_outliers = []
+    for i in range(0, len(data[column_name])):
+        if not ((data[column_name].iloc[i] in IQR_outliers) and (data[column_name].iloc[i] in Zscore_outliers)):
+            non_outliers.append(i)
+
+    data = data.iloc[non_outliers]
+    return data
 
 
 def classifier_model(data, model_name):
@@ -72,6 +104,7 @@ def classifier_model(data, model_name):
                 #Filling missing values
                 if (is_numeric_dtype(data[i])):
                     Missing_values_Handler(data, i)
+                    data = Outlier_Handler(data, i)
         else:
             label_encoder = LabelEncoder()
             label_encoder.fit(data[i])
@@ -124,6 +157,25 @@ def classifier_model(data, model_name):
         model = model.fit(train.drop([Label], axis= 1), train[Label])
         predictions = model.predict(test.drop([Label], axis= 1))
 
+    elif (model_name == "DeepLearning"):
+
+        # define the keras model
+        y = to_categorical(train[Label], num_classes=number_of_classes)
+        number_of_columns = train.drop([Label], axis= 1).shape[1]
+
+        model = Sequential()
+        model.add(Dense(number_of_columns * 2, input_shape=(number_of_columns,), activation='relu'))
+        model.add(Dense(int((number_of_columns * 2 + number_of_classes)/2), activation='relu'))
+        model.add(Dense(number_of_classes, activation="softmax"))
+        # compile the keras model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # fit the keras model on the dataset
+        model.fit(train.drop([Label], axis= 1), y, epochs = 50, batch_size=10)
+        # evaluate the keras model
+        _, accuracy = model.evaluate(train.drop([Label], axis= 1), y)
+
+        prediction = model.predict(test.drop([Label], axis= 1))
+        predictions = np.argmax(prediction, axis=1)
 
     predictions = pd.DataFrame(predictions)
     accuracy = accuracy_score(test[Label], predictions)
@@ -132,7 +184,7 @@ def classifier_model(data, model_name):
     return accuracy, model, Encoders, columns_name
 
 
-def classifier_predictor(model, test_data, Encoders, columns_name):
+def classifier_predictor(model, test_data, Encoders, columns_name, model_name):
 
     test_data = test_data[columns_name]
 
@@ -145,11 +197,14 @@ def classifier_predictor(model, test_data, Encoders, columns_name):
 
             test_data[i] = Encoders[i].transform(test_data[i])
 
-
-    predictions = model.predict(test_data)
+    if model_name == "DeepLearning":
+        prediction = model.predict(test_data)
+        predictions = np.argmax(prediction, axis=1)
+    else:
+        predictions = model.predict(test_data)
 
     return predictions
 
 
 accuracy , model, Encoders, columns_name = classifier_model(data, model_name)
-prediction = classifier_predictor(model, test_data, Encoders, columns_name)
+prediction = classifier_predictor(model, test_data, Encoders, columns_name, model_name)
