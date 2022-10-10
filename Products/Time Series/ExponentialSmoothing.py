5a94c0208fea3d3d3e7059c900f51066fda06d7e
Farshad Seifi <32601295+farshad-seifi@users.noreply.github.com>
2022-10-10 18:10:51 +0330

    Update ExponentialSmoothing.py

--- Products/Time Series/ExponentialSmoothing.py
+++ Products/Time Series/ExponentialSmoothing.py
@@ -6,9 +6,10 @@ from darts.utils.statistics import check_seasonality
 import jdatetime
 
 data = pd.read_excel(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\test_data - Copy.xlsx")
+data = pd.read_excel(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\test_data_month.xlsx")
 
 # data = pd.read_csv(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\Test data\Time series\AEP_hourly.csv")
-data = pd.read_csv(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\Test data\Time series\EKPC_hourly.csv")
+# data = pd.read_csv(r"C:\Users\fafar\OneDrive\Desktop\Desktop\PHD\Prediction_product\Test data\Time series\EKPC_hourly.csv")
 
 data.columns = ["date", "value"]
 
@@ -47,14 +48,22 @@ def date_handler(input):
 
 
 def frequency_finder(data):
-    if((data["date"].iloc[0].hour + data["date"].iloc[1].hour) > 0 ):
-        frequency = "Hourly"
-    elif((data["date"].iloc[1]-data["date"].iloc[0]).days == 1):
-        frequency = "Daily"
-    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 27) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 32)):
-        frequency = "Monthly"
-    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 363) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 366)):
-        frequency = "Yearly"
+    if(len(data) >= 2):
+        if ((data["date"].iloc[0].hour + data["date"].iloc[1].hour) > 0):
+            frequency = "Hourly"
+        elif ((data["date"].iloc[1] - data["date"].iloc[0]).days == 1):
+            frequency = "Daily"
+        elif (((data["date"].iloc[1] - data["date"].iloc[0]).days > 27) and (
+                (data["date"].iloc[1] - data["date"].iloc[0]).days < 32)):
+            frequency = "Monthly"
+        elif (((data["date"].iloc[1] - data["date"].iloc[0]).days > 363) and (
+                (data["date"].iloc[1] - data["date"].iloc[0]).days < 366)):
+            frequency = "Yearly"
+        else:
+            frequency = "None"
+    else:
+        frequency = "None"
+
 
     return frequency
 
@@ -64,6 +73,9 @@ def ExponentialSmoothing_predictor(data, number_of_step_ahead, Confidence_limit)
     The main function for ExponentialSmoothing prediction
     :param data: The main data for prediction
     :param number_of_step_ahead: Number of step ahead for prediction
+    :param Monthly_seasonality: Status of Monthly seasonality
+    :param Weekly_seasonality: Status of weekly seasonality
+    :param frequency: Type of date. Monthly or Daily
     :param Confidence_limit: If be True model returns LCL and UCL
     :return: A 4*number_of_step_ahead dimension dataframe
     """
@@ -71,6 +83,7 @@ def ExponentialSmoothing_predictor(data, number_of_step_ahead, Confidence_limit)
 
     frequency = frequency_finder(data)
 
+    data.reset_index(inplace= True, drop= True)
     data.reset_index(inplace=True)
     data["index"] += 1
     data = data[["index","value"]]
@@ -79,47 +92,56 @@ def ExponentialSmoothing_predictor(data, number_of_step_ahead, Confidence_limit)
     series = TimeSeries.from_dataframe(fill_missing_dates=True, freq=None, df= data, time_col= 'date', value_cols='value')
 
     # Creating model object
-    if (frequency == "Hourly" and (len(data["date"]) > 24) and
+    if (frequency == "Hourly" and (len(data["date"]) >= 2*24) and
             (check_seasonality(series, m = 24, max_lag = len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods= 24)
 
-    elif (frequency == "Hourly" and (len(data["date"]) > 7*24) and
+    elif (frequency == "Hourly" and (len(data["date"]) >= 2*7*24) and
           (check_seasonality(series, m = 7*24, max_lag = len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods= 7 * 24)
 
-    elif (frequency == "Hourly" and (len(data["date"]) > 30 * 24) and
+    elif (frequency == "Hourly" and (len(data["date"]) >= 2*30 * 24) and
           (check_seasonality(series, m=30 * 24, max_lag=len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods=30 * 24)
 
-    elif (frequency == "Daily" and (len(data["date"]) > 7) and
+    elif (frequency == "Daily" and (len(data["date"]) >= 14) and
           (check_seasonality(series, m=7, max_lag=len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods=7)
 
-    elif (frequency == "Daily" and (len(data["date"]) > 30) and
+    elif (frequency == "Daily" and (len(data["date"]) >= 60) and
           (check_seasonality(series, m=30, max_lag=len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods=30)
 
-    elif (frequency == "Monthly" and (len(data["date"]) > 12) and
+    elif (frequency == "Monthly" and (len(data["date"]) >= 24) and
           (check_seasonality(series, m=12, max_lag=len(data["date"]))[0])):
         model = ExponentialSmoothing(seasonal_periods=12)
     else:
-        model = ExponentialSmoothing()
+        model = ExponentialSmoothing(seasonal= None)
 
 
+    if len(data) >= 3 :
+        model.fit(series)
+        prediction = model.predict(number_of_step_ahead, num_samples=1)
+        if Confidence_limit:
+            prediction_for_std = model.predict(number_of_step_ahead, num_samples=1000)
+            prediction_for_std = prediction_for_std.std().values()
 
-    model.fit(series)
-    prediction = model.predict(number_of_step_ahead, num_samples=1)
-    if Confidence_limit:
-        prediction_for_std = model.predict(number_of_step_ahead, num_samples=1000)
-        prediction_for_std = prediction_for_std.std().values()
+        # Creating final alignment for forecasting
+        forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
+        forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
+        forecast[1] = pd.DataFrame(prediction.values())
+        if Confidence_limit:
+            forecast[2] = - 1.96 * pd.DataFrame(prediction_for_std)[0] + pd.DataFrame(forecast[1])[1]
+            forecast[3] = 1.96 * pd.DataFrame(prediction_for_std)[0] + pd.DataFrame(forecast[1])[1]
+    else:
+        prediction = np.zeros(number_of_step_ahead)
+        for i in range(0, number_of_step_ahead):
+            prediction[i] = data["value"].mean()
+        # Creating final alignment for forecasting
+        forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
+        forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
+        forecast[1] = prediction
 
-    # Creating final alignment for forecasting
-    forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
-    forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
-    forecast[1] = pd.DataFrame(prediction.values())
-    if Confidence_limit:
-        forecast[2] = - 1.96 * pd.DataFrame(prediction_for_std)[0] + pd.DataFrame(forecast[1])[1]
-        forecast[3] = 1.96 * pd.DataFrame(prediction_for_std)[0] + pd.DataFrame(forecast[1])[1]
 
     forecast.columns = ["date", "prediction", "LCL", "UCL"]
 
