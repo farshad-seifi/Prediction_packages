ad7138734ac47f0101c5faaaea3cee7b99f3f137
Farshad Seifi <32601295+farshad-seifi@users.noreply.github.com>
2022-10-14 19:12:51 +0330

    Update RNN.py

--- Products/Time Series/RNN.py
+++ Products/Time Series/RNN.py
@@ -38,6 +38,7 @@ def date_handler(input):
                 date = str(date) + "-01"
             else:
                 date = str(date) + "01"
+
         if "/" in str(date) or "-" in str(date):
             date = str(date).replace("-" , "").replace("/" , "")
         if int(str(date)[0:4]) < 1500:
@@ -50,16 +51,25 @@ def date_handler(input):
 
 
 def frequency_finder(data):
-    if((data["date"].iloc[0].hour + data["date"].iloc[1].hour) > 0 ):
-        frequency = "Hourly"
-    elif((data["date"].iloc[1]-data["date"].iloc[0]).days == 1):
-        frequency = "Daily"
-    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 27) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 32)):
-        frequency = "Monthly"
-    elif(((data["date"].iloc[1]-data["date"].iloc[0]).days > 363) and ((data["date"].iloc[1]-data["date"].iloc[0]).days < 366)):
-        frequency = "Yearly"
+    if(len(data) >= 2):
+        if ((data["date"].iloc[0].hour + data["date"].iloc[1].hour) > 0):
+            frequency = "Hourly"
+        elif ((data["date"].iloc[1] - data["date"].iloc[0]).days == 1):
+            frequency = "Daily"
+        elif (((data["date"].iloc[1] - data["date"].iloc[0]).days > 27) and (
+                (data["date"].iloc[1] - data["date"].iloc[0]).days < 32)):
+            frequency = "Monthly"
+        elif (((data["date"].iloc[1] - data["date"].iloc[0]).days > 363) and (
+                (data["date"].iloc[1] - data["date"].iloc[0]).days < 366)):
+            frequency = "Yearly"
+        else:
+            frequency = "None"
+    else:
+        frequency = "None"
+
 
     return frequency
+
 
 def monthdelta(date, delta):
     # This function calculates the difference between to date based on month
@@ -86,146 +96,158 @@ def RNNModel_predictor(data, number_of_step_ahead,RNN_Type):
 
     frequency = frequency_finder(data)
 
-    for i in range(0, len(data["date"])):
-        if (frequency == "Monthly"):
-            year = (monthdelta(datetime.today(), -i)).year
-            month = (monthdelta(datetime.today(), -i)).month
-            day = (monthdelta(datetime.today(), -i)).day
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month)
-        elif (frequency == "Daily"):
-            year = (datetime.today() - timedelta(days=i)).year
-            month = (datetime.today() - timedelta(days=i)).month
-            day = (datetime.today() - timedelta(days=i)).day
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
-        elif (frequency == "Hourly"):
-            year = (datetime.today() - timedelta(hours=i)).year
-            month = (datetime.today() - timedelta(hours=i)).month
-            day = (datetime.today() - timedelta(hours=i)).day
-            hour = (datetime.today() - timedelta(hours=i)).hour
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":00:00"
+    if len(data) >= 21 and frequency != 'None':
+        for i in range(0, len(data["date"])):
+            if (frequency == "Monthly"):
+                year = (monthdelta(datetime.today(), -i)).year
+                month = (monthdelta(datetime.today(), -i)).month
+                day = (monthdelta(datetime.today(), -i)).day
+                data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month)
+            elif (frequency == "Daily"):
+                year = (datetime.today() - timedelta(days=i)).year
+                month = (datetime.today() - timedelta(days=i)).month
+                day = (datetime.today() - timedelta(days=i)).day
+                data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
+            elif (frequency == "Hourly"):
+                year = (datetime.today() - timedelta(hours=i)).year
+                month = (datetime.today() - timedelta(hours=i)).month
+                day = (datetime.today() - timedelta(hours=i)).day
+                hour = (datetime.today() - timedelta(hours=i)).hour
+                data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(
+                    day) + " " + str(hour) + ":00:00"
 
-    series = TimeSeries.from_dataframe(data, 'date', 'value').astype(np.float32)
-    transformer = Scaler()
-    series = transformer.fit_transform(series)
+        series = TimeSeries.from_dataframe(data, 'date', 'value').astype(np.float32)
+        transformer = Scaler()
+        series = transformer.fit_transform(series)
 
-    date = pd.DataFrame(np.zeros((len(data["date"]) + number_of_step_ahead, 1)))
+        date = pd.DataFrame(np.zeros((len(data["date"]) + number_of_step_ahead, 1)))
 
-    for i in range(0, len(data["date"]) + number_of_step_ahead):
-        if (frequency == "Monthly"):
-            year = (monthdelta(datetime.today(), number_of_step_ahead - i)).year
-            month = (monthdelta(datetime.today(), number_of_step_ahead - i)).month
-            day = (monthdelta(datetime.today(), number_of_step_ahead - i)).day
-            date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month)
+        for i in range(0, len(data["date"]) + number_of_step_ahead):
+            if (frequency == "Monthly"):
+                year = (monthdelta(datetime.today(), number_of_step_ahead - i)).year
+                month = (monthdelta(datetime.today(), number_of_step_ahead - i)).month
+                day = (monthdelta(datetime.today(), number_of_step_ahead - i)).day
+                date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month)
 
-        elif (frequency == "Daily"):
-            year = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).year
-            month = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).month
-            day = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).day
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
-            date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
+            elif (frequency == "Daily"):
+                year = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).year
+                month = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).month
+                day = (datetime.today() - timedelta(days=-number_of_step_ahead + i)).day
+                data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
+                date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day)
 
-        elif (frequency == "Hourly"):
-            year = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).year
-            month = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).month
-            day = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).day
-            hour = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).hour
+            elif (frequency == "Hourly"):
+                year = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).year
+                month = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).month
+                day = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).day
+                hour = (datetime.today() - timedelta(hours=-number_of_step_ahead + i)).hour
 
-            data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":00:00"
-            date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":00:00"
+                data["date"].iloc[len(data["date"]) - i - 1] = str(year) + "-" + str(month) + "-" + str(
+                    day) + " " + str(hour) + ":00:00"
+                date[0].iloc[len(date[0]) - i - 1] = str(year) + "-" + str(month) + "-" + str(day) + " " + str(
+                    hour) + ":00:00"
 
+        date.columns = ["date"]
+        date["value"] = 0
+        series_date = TimeSeries.from_dataframe(date, 'date', 'value').astype(np.float32)
+        status = False
 
-    date.columns = ["date"]
-    date["value"] = 0
-    series_date = TimeSeries.from_dataframe(date, 'date', 'value').astype(np.float32)
-    status = False
+        # Adding monthly seasonality to data
+        # We should add weekly too
+        if (frequency == "Hourly" and (len(data["date"]) > 24) and
+                (check_seasonality(series, m=24, max_lag=len(data["date"]))[0])):
 
-    # Adding monthly seasonality to data
-    # We should add weekly too
-    if (frequency == "Hourly" and (len(data["date"]) > 24) and
-            (check_seasonality(series, m = 24, max_lag = len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            hour_series = datetime_attribute_timeseries(year_series, attribute="hour", one_hot=True)
+            covariates = year_series.stack(hour_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        hour_series = datetime_attribute_timeseries(year_series, attribute="hour", one_hot=True)
-        covariates = year_series.stack(hour_series)
-        status = True
+        elif (frequency == "Hourly" and (len(data["date"]) > 7 * 24) and
+              (check_seasonality(series, m=7 * 24, max_lag=len(data["date"]))[0])):
 
-    elif (frequency == "Hourly" and (len(data["date"]) > 7*24) and
-          (check_seasonality(series, m = 7*24, max_lag = len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            week_series = datetime_attribute_timeseries(year_series, attribute="weekday", one_hot=True)
+            covariates = year_series.stack(week_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        week_series = datetime_attribute_timeseries(year_series, attribute="weekday", one_hot=True)
-        covariates = year_series.stack(week_series)
-        status = True
+        elif (frequency == "Hourly" and (len(data["date"]) > 30 * 24) and
+              (check_seasonality(series, m=30 * 24, max_lag=len(data["date"]))[0])):
 
-    elif (frequency == "Hourly" and (len(data["date"]) > 30 * 24) and
-          (check_seasonality(series, m=30 * 24, max_lag=len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            month_series = datetime_attribute_timeseries(year_series, attribute="month", one_hot=True)
+            covariates = year_series.stack(month_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        month_series = datetime_attribute_timeseries(year_series, attribute="month", one_hot=True)
-        covariates = year_series.stack(month_series)
-        status = True
+        elif (frequency == "Daily" and (len(data["date"]) > 7) and
+              (check_seasonality(series, m=7, max_lag=len(data["date"]))[0])):
 
-    elif (frequency == "Daily" and (len(data["date"]) > 7) and
-          (check_seasonality(series, m=7, max_lag=len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            week_series = datetime_attribute_timeseries(year_series, attribute="weekday", one_hot=True)
+            covariates = year_series.stack(week_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        week_series = datetime_attribute_timeseries(year_series, attribute="weekday", one_hot=True)
-        covariates = year_series.stack(week_series)
-        status = True
+        elif (frequency == "Daily" and (len(data["date"]) > 30) and
+              (check_seasonality(series, m=30, max_lag=len(data["date"]))[0])):
 
-    elif (frequency == "Daily" and (len(data["date"]) > 30) and
-          (check_seasonality(series, m=30, max_lag=len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            day_series = datetime_attribute_timeseries(year_series, attribute="day", one_hot=True)
+            covariates = year_series.stack(day_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        day_series = datetime_attribute_timeseries(year_series, attribute="day", one_hot=True)
-        covariates = year_series.stack(day_series)
-        status = True
+        elif (frequency == "Monthly" and (len(data["date"]) > 12) and
+              (check_seasonality(series, m=12, max_lag=len(data["date"]))[0])):
 
-    elif (frequency == "Monthly" and (len(data["date"]) > 12) and
-          (check_seasonality(series, m=12, max_lag=len(data["date"]))[0])):
+            year_data = datetime_attribute_timeseries(series_date, attribute="year")
+            year_series = Scaler().fit_transform(year_data)
+            month_series = datetime_attribute_timeseries(year_series, attribute="month", one_hot=True)
+            covariates = year_series.stack(month_series)
+            status = True
 
-        year_data = datetime_attribute_timeseries(series_date, attribute="year")
-        year_series = Scaler().fit_transform(year_data)
-        month_series = datetime_attribute_timeseries(year_series, attribute="month", one_hot=True)
-        covariates = year_series.stack(month_series)
-        status = True
+        # Creating model object
+        # Hyperparameters should be optimized.
+        my_model = RNNModel(
+            model=RNN_Type,
+            hidden_dim=20,
+            dropout=0,
+            batch_size=16,
+            n_epochs=100,
+            optimizer_kwargs={"lr": 1e-3},
+            model_name="data_RNN",
+            log_tensorboard=True,
+            random_state=42,
+            training_length=20,
+            input_chunk_length=14,
+            force_reset=True,
+            save_checkpoints=True)
 
+        if (status):
+            my_model.fit(series, future_covariates=covariates.astype(np.float32))
+        else:
+            my_model.fit(series)
 
-    # Creating model object
-    # Hyperparameters should be optimized.
-    my_model = RNNModel(
-                        model=RNN_Type,
-                        hidden_dim=20,
-                        dropout=0,
-                        batch_size=16,
-                        n_epochs=50,
-                        optimizer_kwargs={"lr": 1e-3},
-                        model_name="data_RNN",
-                        log_tensorboard=True,
-                        random_state=42,
-                        training_length=20,
-                        input_chunk_length=14,
-                        force_reset=True,
-                        save_checkpoints=True)
+        # Creating final alignment for forecasting
 
+        prediction = my_model.predict(number_of_step_ahead, num_samples=1)
+        forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
+        forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
+        forecast[1] = pd.DataFrame(transformer.inverse_transform(prediction).values())
+        forecast.columns = ["date", "prediction", "LCL", "UCL"]
 
-    if (status):
-        my_model.fit(series, future_covariates=covariates.astype(np.float32))
     else:
-        my_model.fit(series)
+        prediction = np.zeros(number_of_step_ahead)
+        for i in range(0, number_of_step_ahead):
+            prediction[i] = data["value"].mean()
+        # Creating final alignment for forecasting
+        forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
+        forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
+        forecast[1] = prediction
+        forecast.columns = ["date", "prediction", "LCL", "UCL"]
 
-    # Creating final alignment for forecasting
-
-    prediction = my_model.predict(number_of_step_ahead, num_samples = 1)
-    forecast = pd.DataFrame(np.zeros((number_of_step_ahead, 4)))
-    forecast[0] = [x + len(data["value"]) for x in range(1, 1 + number_of_step_ahead)]
-    forecast[1] = pd.DataFrame(transformer.inverse_transform(prediction).values())
-    forecast.columns = ["date", "prediction", "LCL", "UCL"]
 
     return forecast
 
